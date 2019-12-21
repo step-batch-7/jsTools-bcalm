43922ffd21b51d8505cb799ff6d6d2ca9bf0192b
Vikram <58028680+bcalm@users.noreply.github.com>
2019-12-21 17:30:23 +0530

    Added: formatMessage function.

--- src/cutLib.js
+++ src/cutLib.js
@@ -1,6 +1,10 @@
+const formatMessage = function(fileContents, delimiter) {
+  return fileContents.map(line => line[0].split(delimiter));
+};
+
 const displayMessage = function(content, number) {
   const message = content.map(text => text[number - 1]);
   return message.join("\n");
 };
 
-exports.displayMessage = displayMessage;
+module.exports = { formatMessage, displayMessage };
--- test/testCutLib.js
+++ test/testCutLib.js
@@ -1,13 +1,24 @@
 const assert = require("chai").assert;
-const displayMessage = require("../src/cutLib.js").displayMessage;
+const lib = require("../src/cutLib.js");
 
 describe("#displayMessage", () => {
   it("should display the nth field of content", () => {
     const content = [["h", "llo"], ["how ar", "you"], ["I am fin"]];
-    const actual = displayMessage(content, 1);
+    const actual = lib.displayMessage(content, 1);
     const expected = "h\nhow ar\nI am fin";
     assert.strictEqual(actual, expected);
   });
+});
 
-  it
+describe("#formatMessage", () => {
+  it("should separate each line content by delimiter", () => {
+    const fileContent = [["hello"], ["how are you"], ["I am fine."]];
+    const actual = lib.formatMessage(fileContent, "e");
+    const expected = [
+      ["h", "llo"],
+      ["how ar", " you"],
+      ["I am fin", "."]
+    ];
+    assert.deepStrictEqual(actual, expected);
+  });
 });
