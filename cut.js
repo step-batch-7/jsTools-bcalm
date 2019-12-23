7b26409a93b76cd5abbcd4c59b60c36efcb4fb0e
Vikram <58028680+bcalm@users.noreply.github.com>
2019-12-23 15:16:42 +0530

    Added apptests

--- cut.js
+++ cut.js
@@ -4,7 +4,7 @@ const performAction = require("./src/cutLib.js").performAction;
 const main = function() {
   const cmdLineArgs = process.argv.slice(2);
   const fileFunction = { readFile: fs.readFileSync, existsFile: fs.existsSync };
-  console.log(performAction(fileFunction, cmdLineArgs));
+  process.stdout.write(performAction(fileFunction, cmdLineArgs));
 };
 
 main();
