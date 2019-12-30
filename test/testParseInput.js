3d673f7819f63f2dc2d3232001aac1a7c7c3a64a
Vikram <58028680+bcalm@users.noreply.github.com>
2019-12-30 10:31:16 +0530

    Modified: testParseInput file on the basis of eslint.

--- test/testParseInput.js
+++ test/testParseInput.js
@@ -1,38 +1,38 @@
-const assert = require("chai").assert;
-const { parseInput, getOptions } = require("../src/parseInput.js");
+const assert = require('chai').assert;
+const { parseInput, getOptions } = require('../src/parseInput.js');
 
-describe("#parseInput", () => {
-  it("should give a object which includes all options as key and their values also", () => {
-    const cmdLineArgs = ["-d", "e", "-f", "1", "one.txt"];
+describe('#parseInput', () => {
+  it('should give a object which includes all options and their values', () => {
+    const cmdLineArgs = ['-d', 'e', '-f', '1', 'one.txt'];
     const actual = parseInput(cmdLineArgs);
-    const expected = { delimiter: "e", fieldValue: "1", fileName: "one.txt" };
+    const expected = { delimiter: 'e', fieldValue: '1', fileName: 'one.txt' };
     assert.deepStrictEqual(actual, expected);
   });
 
-  it("should give undefined if file is not given", () => {
-    const cmdLineArgs = ["-d", "e", "-f", "1"];
+  it('should give undefined if file is not given', () => {
+    const cmdLineArgs = ['-d', 'e', '-f', '1'];
     const actual = parseInput(cmdLineArgs);
-    const expected = { delimiter: "e", fieldValue: "1", fileName: undefined };
+    const expected = { delimiter: 'e', fieldValue: '1', fileName: undefined };
     assert.deepStrictEqual(actual, expected);
   });
 });
 
-describe("#getOptions", () => {
-  it("should give array which includes option", () => {
-    const actual = getOptions([], "-d");
-    const expected = ["-d"];
+describe('#getOptions', () => {
+  it('should give array which includes option', () => {
+    const actual = getOptions([], '-d');
+    const expected = ['-d'];
     assert.deepStrictEqual(actual, expected);
   });
 
-  it("should make pair of options and their values", () => {
-    const actual = getOptions(["-d"], "1");
-    const expected = [["-d", "1"]];
+  it('should make pair of options and their values', () => {
+    const actual = getOptions(['-d'], '1');
+    const expected = [['-d', '1']];
     assert.deepStrictEqual(actual, expected);
   });
 
-  it("should not make pair of given element if last element is not an array", () => {
-    const actual = getOptions([["-d", "1"]], "hello.txt");
-    const expected = [["-d", "1"]];
+  it('should not make pair of given element if it is not an option', () => {
+    const actual = getOptions([['-d', '1']], 'hello.txt');
+    const expected = [['-d', '1']];
     assert.deepStrictEqual(actual, expected);
   });
 });
